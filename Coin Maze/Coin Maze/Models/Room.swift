98999a3379ebf56e2351f0d54804aa32c53a3e46
Angel <angelbuenrostro@outlook.com>
2019-09-21 10:40:10 -0400

    Swift UI project creation

---
+++ Coin Maze/Coin Maze/Models/Room.swift
@@ -0,0 +1,12 @@
+//
+//  Room.swift
+//  Coin Maze
+//
+//  Created by Angel Buenrostro on 9/21/19.
+//  Copyright © 2019 Angel Buenrostro. All rights reserved.
+//
+
+import Foundation
+
+struct Room:
+
